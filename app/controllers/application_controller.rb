89df72f5324dbdf2cdee48ec7dbc31cf29419934
programmingdogw <59106983+programmingdogw@users.noreply.github.com>
2020-04-15 17:24:00 +0900

    ベーシック認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
+
 
   protected
   
@@ -8,4 +10,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :age])
   end
   
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'dog' && password == '7777'
+    end
+  end
+
 end
