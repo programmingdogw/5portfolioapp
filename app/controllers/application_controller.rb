98432cf31c2b3d90602f9e69e59e0e3ca1a6fc10
programmingdogw <59106983+programmingdogw@users.noreply.github.com>
2020-04-15 17:27:46 +0900

    ローカルでベーシック認証する意味ないからデプロイ時に環境変数使ってやる

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :basic_auth
+  
 
 
   protected
@@ -10,10 +10,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :age])
   end
   
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == 'dog' && password == '7777'
-    end
-  end
+  
 
 end
