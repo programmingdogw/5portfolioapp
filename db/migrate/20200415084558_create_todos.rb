a417df9087dc1213f10ab0dd0e2e64db5c86c81a
programmingdogw <59106983+programmingdogw@users.noreply.github.com>
2020-04-15 18:05:54 +0900

    ancestryカラム追加

--- db/migrate/20200415084558_create_todos.rb
+++ db/migrate/20200415084558_create_todos.rb
@@ -1,7 +1,7 @@
 class CreateTodos < ActiveRecord::Migration[5.2]
   def change
     create_table :todos do |t|
-      #Add
+    
       t.bigint :user_id,    null: false, foreign_key: true
       t.text :lead_text,     null: false
       t.text :content_text,  null: false
