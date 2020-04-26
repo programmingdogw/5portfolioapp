8694073f11cbef97190bb520ea010e5581903c5b
programmingdogw <59106983+programmingdogw@users.noreply.github.com>
2020-04-26 17:20:38 +0900

    Revert "revert予定"
    This reverts commit 4c491086cfec529f5ffd39892fb52c801790da52.

--- db/migrate/20200415084531_create_items.rb
+++ db/migrate/20200415084531_create_items.rb
@@ -17,7 +17,6 @@ class CreateItems < ActiveRecord::Migration[5.2]
       t.boolean :auction
       t.boolean :dealing
       t.boolean :sold
-      t.integer :order
 
       t.text :parentcategory,   null: false
       t.integer :childcategory,   null: false
