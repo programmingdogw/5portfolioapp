86249b81b0f151f84dd83d60f70a9c914b471355
programmingdogw <59106983+programmingdogw@users.noreply.github.com>
2020-04-26 18:36:59 +0900

    homeのajaxを特定アクションから出した

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
 
   def search
+    @user = current_user
     @items = Item.firstsearch(params[:search])
   
     
@@ -66,20 +67,23 @@ class HomeController < ApplicationController
        @category_parent_array << parent.name
      end
      
-    # 親カテゴリーが選択された後に動くアクション
-    def get_category_children
-      #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
-      @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
-    end
-
-    # 子カテゴリーが選択された後に動くアクション
-    def get_category_grandchildren
-      #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
-      @category_grandchildren = Category.find("#{params[:child_id]}").children
-    end
-
   end
 
+
+
+  # 親カテゴリーが選択された後に動くアクション
+  def get_category_children
+    #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
+    @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
+  end
+
+  # 子カテゴリーが選択された後に動くアクション
+  def get_category_grandchildren
+    #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
+    @category_grandchildren = Category.find("#{params[:child_id]}").children
+  end
+
+  
   def unexpectederrors
     # 予期せぬ動作が起きた場合に飛ぶページ用
     # エラーハンドリングの遷移先として置いてる
