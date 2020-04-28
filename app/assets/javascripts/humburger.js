302587da174d3bc3578705b9ec56bfd94a399aeb
programmingdogw <59106983+programmingdogw@users.noreply.github.com>
2020-04-28 18:12:48 +0900

    名前の付け方

--- app/assets/javascripts/humburger.js
+++ app/assets/javascripts/humburger.js
@@ -1,7 +1,7 @@
 $(document).on('turbolinks:load', ()=> {
 
-  $('.nav_toggle').on('click', function () {
-    $('.nav_toggle, .nav').toggleClass('show');
+  $('.navtoggle').on('click', function () {
+    $('.navtoggle, .nav').toggleClass('show');
   });
 
 });
