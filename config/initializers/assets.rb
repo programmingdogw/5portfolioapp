f986626f9d58c98ca521170289a3d81503ba41e1
programmingdogw <59106983+programmingdogw@users.noreply.github.com>
2020-04-29 10:47:10 +0900

    カテゴリshow切り分け

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -26,6 +26,8 @@ Rails.application.config.assets.precompile += %w( items_new.scss )
 Rails.application.config.assets.precompile += %w( items_edit.scss )
 
 Rails.application.config.assets.precompile += %w( categories_index.scss )
+Rails.application.config.assets.precompile += %w( categories_show.scss )
+
 
 Rails.application.config.assets.precompile += %w( search.scss )
 
