581eec02a766b6211d4754ca8d1b4284386dbcc9
programmingdogw <59106983+programmingdogw@users.noreply.github.com>
2020-05-02 11:01:13 +0900

    revert

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -34,7 +34,6 @@ Rails.application.config.assets.precompile += %w( items_show.scss )
 Rails.application.config.assets.precompile += %w( categories_index.scss )
 Rails.application.config.assets.precompile += %w( categories_show.scss )
 
-Rails.application.config.assets.precompile += %w( public/images/* )
 
 
 
